4bcd98707641985db3f1b4a526c8137c5e3bfec4
harsh-c <harsh_chauhan94@hotmail.com>
2015-12-22 17:51:12 -0800

    Presentation 14

---
+++ Pres14/slide52/main.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main(){
+        firstName:= "Harsh"
+        lastName:= "Chauhan"
+        fmt.Println("The length of my name is ", len(firstName) + len(lastName))
+}
